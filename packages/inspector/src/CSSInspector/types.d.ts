95b0bfaac4531e8bab483eb0baf322e5394749ef
dolanoadriano <adrian.trabka98@gmail.com>
2023-09-24 16:28:05 +0200

    Add checker package

--- packages/inspector/src/CSSInspector/types.d.ts
+++
@@ -1,5 +0,0 @@
-export type StyleDeclarationSchemaValue = string | RegExp | boolean;
-export type StyleDeclarationSchemaCallback = (value: string) => boolean;
-export type StyleDeclarationSchema = Partial<{
-    [key in keyof CSSStyleRule["style"]]: StyleDeclarationSchemaValue | StyleDeclarationSchemaCallback;
-}>;
--- packages/inspector/src/utils/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.kebabize = void 0;
-const kebabize = (str) => str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? "-" : "") + $.toLowerCase());
-exports.kebabize = kebabize;
-//# sourceMappingURL=index.js.map
