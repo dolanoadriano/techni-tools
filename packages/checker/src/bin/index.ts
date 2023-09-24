e6697efe2634fcd53a5701a564f6a04e14197f1b
dolanoadriano <adrian.trabka98@gmail.com>
2023-09-24 17:09:16 +0200

    Add testPathIgnorePatterns to jest CLI

--- packages/checker/src/bin/index.ts
+++ packages/checker/src/bin/index.ts
@@ -10,9 +10,19 @@ program
   .option("-t --test <id>")
   .action(async (options) => {
     const { project, test } = options;
-    if (!project) throw new Error("-p undefined");
-    if (!test) throw new Error("-t undefined");
-    await run([`--testPathPattern=${project}`, `-t=${test}`]);
+    if (!project) {
+      console.error("-p undefined");
+      process.exit(-1);
+    }
+    if (!test) {
+      console.error("-t undefined");
+      process.exit(-1);
+    }
+    await run([
+      `--testPathPattern=${project}`,
+      `-t=${test}`,
+      `-testPathIgnorePatterns='/node_modules\/(?!@techni-tools)(.*)'`,
+    ]);
   });
 
 program.parse();
