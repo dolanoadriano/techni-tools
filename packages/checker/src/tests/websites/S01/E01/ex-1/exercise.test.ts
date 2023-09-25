bbdd7fe0d526a7528a7e86b60806fe80ff3e5f98
dolanoadriano <adrian.trabka98@gmail.com>
2023-09-25 21:43:04 +0200

    Update tests

--- packages/checker/src/tests/websites/S01/E01/ex-1/exercise.test.ts
+++ packages/checker/src/tests/websites/S01/E01/ex-1/exercise.test.ts
@@ -1,5 +1,5 @@
+import { loadHTML } from "@techni-tools/inspector";
 import { expect } from "chai";
-import fs from "fs";
 import { JSDOM } from "jsdom";
 import { before, describe } from "mocha";
 import path from "path";
@@ -11,16 +11,53 @@ describe(`Exercise ${exerciseNumber}`, () => {
   let document: Document;
 
   before(() => {
-    const htmlContent = fs.readFileSync(
-      path.resolve(process.cwd(), "..", "index.html"),
-      "utf-8"
+    const htmlContent = loadHTML(
+      path.resolve(process.cwd(), "..", "index.html") as any
     );
-    dom = new JSDOM(htmlContent);
-    document = dom.window.document;
   });
 
   it("should have an <h1> element in the <body>", () => {
     const h1Element = document.querySelector("body > h1");
     expect(h1Element).not.null;
+  });
+
+  it("should have an <span> element in the <body>", () => {
+    const spanElement = document.querySelector("body > span");
+    expect(spanElement).not.null;
+  });
+
+  it("should have a text inside <h1> and <span>", () => {
+    const h1Element = document.querySelector("body > h1");
+    const spanElement = document.querySelector("body > span");
+    expect(h1Element).not.null;
+    expect(spanElement).not.null;
+    expect(spanElement?.textContent?.trim()).not.be("");
+    expect(h1Element?.textContent?.trim()).not.be("");
+  });
+
+  it("should have a Google Font link in the <head>", () => {
+    const linkElement = document.querySelector(
+      `head > link[href^="https://fonts.googleapis.com/css"]`
+    );
+    expect(linkElement).not.null;
+  });
+
+  it("should have an <style> in the <head>", () => {
+    const styleElement = document.querySelector(`head > style`);
+    expect(styleElement).not.null;
+  });
+
+  it("should have a CSS rule for the 'body' selector with a specified 'font-family' property", () => {
+    const bodyRule = findCssRuleBySelector("body");
+    const linkElement = document.querySelector(
+      `head > link[href^="https://fonts.googleapis.com/css"]`
+    );
+    const href = linkElement?.getAttribute("href");
+    const fonts = extractFontsFromGoogleFontsUrl(href);
+    expect(
+      fonts?.some((font) =>
+        bodyRule?.style.getPropertyValue("font-family").includes(font)
+      )
+    ).be(true);
   });
 });
