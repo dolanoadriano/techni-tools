07f6894e7d74dc643b72714ad84e82458ce86203
techniadrian <adrian.trabka@technischools.com>
2024-03-23 13:56:36 +0100

    Add json middleware

--- packages/postman/run.js
+++ packages/postman/run.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "lib")));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "lib", "index.html"));
