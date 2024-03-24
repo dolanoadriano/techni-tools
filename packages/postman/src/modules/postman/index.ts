e2713c75385bc2c96eaf0f3c95f2e27b7fec4eaf
techniadrian <adrian.trabka@technischools.com>
2024-03-24 20:55:21 +0100

    Remove proxy

--- packages/postman/src/modules/postman/index.ts
+++ packages/postman/src/modules/postman/index.ts
@@ -9,11 +9,12 @@ postman.interceptors.request.use(extendConfig);
 postman.interceptors.response.use(extendResponse, extendErrorResponse);
 
 postman.interceptors.response.use(decodeResponse, decodeErrorResponse);
-
+/*
 postman.interceptors.request.use((config) => {
   config.headers.set("Postman-Target-Url", config.url);
   config.url = `/api/proxy`;
   return config;
 });
+*/
 
 export default postman;
